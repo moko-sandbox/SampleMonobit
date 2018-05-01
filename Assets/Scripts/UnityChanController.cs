48b1963fa7dfc9648c5f0997a8e148be86278426
moko-ist <yukie.hr@gmail.com>
2018-05-01 18:59:25 +0900

    Add unity chan controller sample

---
+++ Assets/Scripts/UnityChanController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Animator))]
+
+public class UnityChanController : MonoBehaviour
+{
+	[SerializeField] private float mSpeedScale = 5.0f;
+	private Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+		float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		Vector3 addPos = new Vector3(horizontal, vertical, 0.0f) * mSpeedScale;
+		transform.position += addPos * Time.deltaTime;
+	}
+
+}
