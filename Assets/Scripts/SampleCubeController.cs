a018f9cfa057d63631f91396207286ca8f3e965c
moko-ist <yukie.hr@gmail.com>
2018-05-01 18:32:49 +0900

    Add virtual joystick and sample code to move cube by it

---
+++ Assets/Scripts/SampleCubeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CubeController : MonoBehaviour
+{
+	[SerializeField] private float mSpeedScale = 5.0f;
+
+	void Update ()
+	{
+		float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+		float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		Vector3 addPos = new Vector3(horizontal, vertical, 0.0f) * mSpeedScale;
+		transform.position += addPos * Time.deltaTime;
+
+		if (CrossPlatformInputManager.GetButton("Jump"))
+		{
+			GetComponent<Renderer>().material.color = Color.red;
+			;
+		}
+		else
+		{
+			GetComponent<Renderer>().material.color = Color.white;
+		}
+	}
+}
